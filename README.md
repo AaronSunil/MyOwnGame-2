536a6ea319e55dd3abb69d243d30c3d3f5c3bc0e
AaronSunil <72694245+AaronSunil@users.noreply.github.com>
2021-06-13 10:14:23 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# MyOwnGame-2
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ js/LabCellWall.js
@@ -0,0 +1,11 @@
+class LabCellWall extends Wall
+{
+   constructor(x,y,height,angle)
+   {
+        super(x,y,20,height,angle);
+    }
+    display()
+    {
+        fill(0,0,0)
+    }
+}
